5093f15e09aa09d65bc1d1cde56716b29a7d1eff
Ciro Galvão <51967334+cgalvaojr@users.noreply.github.com>
2024-11-09 21:37:25 -0300

    chore: configuring inertia with Vue3.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/example/{user}', [ExampleController::class, 'show']);
