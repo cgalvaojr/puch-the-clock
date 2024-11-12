7427b7c60f9e793bcc3d9dc9c00d534388697496
Ciro Galvão <51967334+cgalvaojr@users.noreply.github.com>
2024-11-11 23:43:48 -0300

    feat: Request validation

--- app/Http/Controllers/PlatformController.php
+++ app/Http/Controllers/PlatformController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePlatformRequest;
 use App\Http\Resources\PlatformResource;
 use App\Services\PlatformService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class PlatformController extends Controller
 {
     public function __construct(private readonly PlatformService $service)
     {}
-    public function store(Request $request): PlatformResource|JsonResponse
+    public function store(StorePlatformRequest $request): PlatformResource|JsonResponse
     {
+        $validatedData = $request->validated();
+
         try {
-            $platformData = $this->service->store($request->all());
+            $platformData = $this->service->store($validatedData);
             return new PlatformResource((object)$platformData->platform);
         } catch(\RuntimeException $exception) {
             return response()->json([
